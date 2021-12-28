d5b706860ca5deb1cd31dbabb0166375b7ac4174
Руслан Любкин <95690726+Ruslan434@users.noreply.github.com>
2021-12-28 14:28:42 +0300

    Операторы break, continue и else задача № 2

--- Interactiv_exemple.py
+++ Interactiv_exemple.py
@@ -1,14 +1,16 @@
-p = [0] * 10
-while sum(p) < 5 :
-    z = int(input("вводи целые числа по очереди от 0 до 9_"))
-    if z > 9:
-        continue
-    elif p[z] == 1:
+p = 1
+z = 1
+while z :
+    z = int(input("вводи целые числа по очереди "))
+    if z == 0:
         break
-    p[z] = 1
+    elif z < 0:
+        continue
+
+    p = p * z
 else:
     print('так работают операторы continue, break, else')
-print(*p)
+print(p)
 
 
 
