11af047ce6dece8ff2d7473b4d6b0123bfda7848
Руслан Любкин <95690726+Ruslan434@users.noreply.github.com>
2021-12-28 14:05:22 +0300

    #Пример работы операторов цикла Операторы break, continue и else задача № 1

--- Interactiv_exemple.py
+++ Interactiv_exemple.py
@@ -1,11 +1,15 @@
 p = [0] * 10
 while sum(p) < 5 :
-    z = int(input("вводи целые числа по очереди_ от 0 до 9"))
-    if 0 <= z <= 9:
-        if p[z] == 1:
-            continue
-        p[z] = 1
+    z = int(input("вводи целые числа по очереди от 0 до 9_"))
+    if z > 9:
+        continue
+    elif p[z] == 1:
+        break
+    p[z] = 1
+else:
+    print('так работают операторы continue, break, else')
 print(*p)
+
 
 
 
