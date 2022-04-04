2e140aac8b4fabbc6a33e1b6b87e24b1404160e0
Руслан Любкин <95690726+Ruslan434@users.noreply.github.com>
2022-04-04 18:54:59 +0300

    Вложенные циклы. Примеры

--- Вложенные циклы.py
+++ Вложенные циклы.py
@@ -4,9 +4,23 @@ for i in range (1,4):
     print()
 
 
-a = [[1,2,3,4,],[2,3,4,5],[3,4,5,6]]
+a = [[1,2,3,4,], [2,3,4,5], [3,4,5,6]]
+b = [[1,1,1,100],[2,2,2,200],[3,3,3,300]]
+c = []
+for i,lst in enumerate(a):
+    r = []
+    for j,x in enumerate(lst):
+        r.append(x + b[i][j])
+    c.append(r)
+print(c)
 
-for lst in a:
-    for x in lst:
-        print(x, type(x), end=' ')
+
+A = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+
+for i in range(len(A)):
+    for j in range(i+1, len(A)):
+        A[i][j],A[j][i] = A[j][i],A[i][j]
+for r in A:
+    for x in r:
+        print(x, end='\t')
     print()
