b07f1c3786ab000c33075d3a8d198acf13224785
Руслан Любкин <95690726+Ruslan434@users.noreply.github.com>
2022-04-15 17:08:39 +0300

    Вложенные циклы. Задача 5

--- Interactiv_exemple.py
+++ Interactiv_exemple.py
@@ -1,13 +1,11 @@
-lst = map(int,list(input()))
-f = iter(lst)
-print(next(f),end=" ")
-print(next(f),end=" ")
-print(next(f),end=" ")
-print(next(f),end=" ")
-
-
-
-
-
-
-
+y = [[2, 3, 4, 5, 6], [4, 2, 7, 8, 9], [4, 7, 2, 0, 4], [5, 8, 0, 2, 1], [6, 9, 4, 1, 2]]
+f = 0
+for i in range(len(y)):
+    for g in range(i + 1, len(y)):
+        if y[i][g] == y[g][i]:
+            f = 1
+        else:
+            f = -1
+            print( "НЕТ")
+            break
+        print("ДА")
