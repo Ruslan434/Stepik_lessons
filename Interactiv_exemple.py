3c2ee59c710bc78d87399e67d2d6a1cac4be1380
Ruslan434 <1533207@mail.ru>
2022-04-16 00:52:16 +0300

    ctl+alt+l для соответствия pip

--- Interactiv_exemple.py
+++ Interactiv_exemple.py
@@ -1,11 +1,9 @@
-y = [[2, 3, 4, 5, 6], [4, 2, 7, 8, 9], [4, 7, 2, 0, 4], [5, 8, 0, 2, 1], [6, 9, 4, 1, 2]]
-f = 0
-for i in range(len(y)):
-    for g in range(i + 1, len(y)):
-        if y[i][g] == y[g][i]:
-            f = 1
-        else:
-            f = -1
-            print( "НЕТ")
+lst_in = [[2, 2, 4, 5, 6], [3, 2, 7, 8, 9], [4, 7, 2, 0, 4], [5, 8, 0, 2, 1], [6, 9, 4, 1, 2]]
+c = "ДА"
+for i in range(len(lst_in)):
+    for t in range(i + 1, len(lst_in)):
+        if lst_in[i][t] != lst_in[t][i]:
+            c = "НЕТ"
             break
-        print("ДА")
+
+print(c)
